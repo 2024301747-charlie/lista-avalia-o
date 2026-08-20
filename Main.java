c685bff29ac0a1bc8b31477eee5368961b5fb602
ProfeRaissa <raissa.scheeren@canoas.ifrs.edu.br>
2026-08-20 11:56:35 -0300

    metodos em serie, calc, notas do estudante

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+
+    public static void main(String[] args) {
+       Estudante estudante = new Estudante("Pudim");
+       
+        estudante.setNotas(new double[5]);
+    }
+}
