b6f69872828cb59a7db3110d5c44a835099c3581
2024301747-charlie <2024301747@aluno.canoas.ifrs.edu.br>
2026-08-24 11:30:05 -0300

    Update1 EstudanteTeste.java

--- EstudanteTeste.java
+++ EstudanteTeste.java
@@ -1,8 +1,19 @@
+    import java.util.Scanner;
 public class EstudanteTeste{
-
     public static void main(String[] args) {
-       Estudante estudante = new Estudante("Pudim");
-       
-        estudante.setNotas(new double[5]);
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.print("\nQuantos estudantes você quer cadastrar?");
+        int quant = leitor.nextInt();
+        leitor.nextLine();
+
+        Estudante[] turma = new Estudante[quant];
+
+        for (int i = 0; i < turma.length; i++) {
+            System.out.print("\nDigite o nome do estudante " + (i + 1) + ": ");
+            String nomeDigitado = leitor.nextLine();
+
+            turma[i] = new Estudante(nomeDigitado);
+        }
     }
 }
