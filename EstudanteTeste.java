e6c063e2b1426e1e8e0164eed8af68258407adc8
2024301747-charlie <2024301747@aluno.canoas.ifrs.edu.br>
2026-08-24 14:20:06 -0300

    Update 2 EstudanteTeste.java

--- EstudanteTeste.java
+++ EstudanteTeste.java
@@ -1,19 +1,36 @@
-    import java.util.Scanner;
+import java.util.Scanner;
+import java.util.ArrayList;
 public class EstudanteTeste{
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
+        
+        ArrayList<String> aprovados = new ArrayList<>();
 
-        System.out.print("\nQuantos estudantes você quer cadastrar?");
+        System.out.print("\nQuantos estudantes são na turma? ");
         int quant = leitor.nextInt();
         leitor.nextLine();
 
-        Estudante[] turma = new Estudante[quant];
+        for (int i = 0; i < quant; i++) {
+            System.out.print("\nNome do estudante "+(i+1)+": ");
+            String nome = leitor.nextLine();
 
-        for (int i = 0; i < turma.length; i++) {
-            System.out.print("\nDigite o nome do estudante " + (i + 1) + ": ");
-            String nomeDigitado = leitor.nextLine();
+            Estudante estudante = new Estudante(nome);
+            estudante.insereNotas();
+            if (estudante.calc()>=6.0) {
+                aprovados.add(estudante.getNome());   
+            } 
+        }
 
-            turma[i] = new Estudante(nomeDigitado);
+        if(!aprovados.isEmpty()){
+            System.out.println("\n\tLISTA DE APROVADOS ("+aprovados.size()+")\t");
+            for (String nomeAprovado : aprovados) {
+                System.out.println("\t "+nomeAprovado);
+            }
+
+            leitor.close();
+        }
+        else {
+            System.out.println("\nNenhum estudante obteve média superior a 6.0.");
         }
     }
 }
