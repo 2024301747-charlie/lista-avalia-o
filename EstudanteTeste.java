98fe9a8584e41c3b5258fa588a62ba9df6858519
2024301747-charlie <2024301747@aluno.canoas.ifrs.edu.br>
2026-08-24 23:26:59 -0300

    Update EstudanteTeste.java

--- EstudanteTeste.java
+++ EstudanteTeste.java
@@ -1,8 +1,9 @@
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Scanner;
 public class EstudanteTeste{
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
+        int option;
         
         ArrayList<String> aprovados = new ArrayList<>();
 
@@ -10,16 +11,43 @@ public class EstudanteTeste{
         int quant = leitor.nextInt();
         leitor.nextLine();
 
-        for (int i = 0; i < quant; i++) {
-            System.out.print("\nNome do estudante "+(i+1)+": ");
-            String nome = leitor.nextLine();
+        Estudante[] turma = new Estudante[quant];
+        for(int i=0; i<quant;i++){
+        System.out.print("\nNome do estudante "+(i+1)+": ");
+        String nome = leitor.nextLine();
 
-            Estudante estudante = new Estudante(nome);
-            estudante.insereNotas();
-            if (estudante.calc()>=6.0) {
-                aprovados.add(estudante.getNome());   
-            } 
         }
+        do{
+            System.out.print("\nVoce gostaria de media simples(1) ou media ponderada(2)? ");
+            option = Integer.parseInt(leitor.nextLine());
+            switch(option){ 
+                case 1:
+                    for (int i = 0; i < quant; i++) {
+                            turma[i].insereNotas();
+                            if (turma[i].calc()>=6.0) {
+                                aprovados.add(turma[i].getNome());   
+                            } 
+                        }
+                        break;
+                case 2:
+                    Integer[] pesos = new Integer[5];
+                    for(int i=0;i<5;i++){
+                        System.out.print("\nInsira as notas:");
+                        pesos[i] = Integer.parseInt(leitor.nextLine());
+                    }
+                    for(int i=0;i<quant;i++){
+                        if (turma[i].calc(pesos)>=6.0) {
+                                aprovados.add(turma[i].getNome());   
+                        } 
+                    }
+                    break;
+                default:
+                    System.out.print("\nNUMERO INVALIDO");
+                    option=3;
+                    break;
+            }
+
+        }while(option==3);
 
         if(!aprovados.isEmpty()){
             System.out.println("\n\tLISTA DE APROVADOS ("+aprovados.size()+")\t");
@@ -27,10 +55,11 @@ public class EstudanteTeste{
                 System.out.println("\t "+nomeAprovado);
             }
 
-            leitor.close();
+           
         }
         else {
             System.out.println("\nNenhum estudante obteve média superior a 6.0.");
         }
+         leitor.close();
     }
 }
